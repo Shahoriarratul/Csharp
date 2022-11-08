912d82515e0ac395381119adc3361ac3ac52d984
shahoriar Ratul <shahoriar.ratul@gmail.com>
2022-11-08 23:07:35 +0600

    add

--- assignment 2/443 A/443 A/Program.cs
+++ assignment 2/443 A/443 A/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String str = Console.ReadLine();
+char[] spearator = { '{',',','}'};
+String[] strlist = str.Split(spearator);
+int cnt = 0;
+
+for (int i = 1; i <(strlist.Length-1); i++)
+{
+    cnt++;
+    for (int j = 1; j <(strlist.Length - 1); j++)
+    {
+        if (i!=j && strlist[i] == strlist[j])
+        {
+
+           
+        }
+        else
+        {
+            cnt++;
+        }
+       
+    }
+    Console.WriteLine(cnt);
+}
+Console.WriteLine(cnt);
