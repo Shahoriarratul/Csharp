ddd9469c303447aa0c0c430d0c957340549316bf
Shahoriar Ratul <Shahoriar.ratul@gmail.com>
2022-11-28 22:50:28 +0600

    3 question

--- Exam2/Task1/OrderHistory.cs
+++ Exam2/Task1/OrderHistory.cs
@@ -3,28 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Task1
 {
     public class OrderHistory
     {
-        dynamic persons = new (string name, int age)[]
-        {
-            ("jalaluddin", 41),
-            ("tareq", 33),
-            ("hasan", 52),
-            ("rashed", 21),
-            ("monir", 45)
-        };
-        dynamic  orders = new (string name, double amount)[]
-        {
-            ("monir", 300.5),
-            ("rashed", 20.5),
-            ("tareq", 29.9),
-            ("hasan", 17.7),
-            ("jalaluddin", 30.2)
+        
+        dynamic persons = new[] { 
+            new { name = "jalaluddin", age = 41 },  
+            new{ name = "tareq", age = 33 },
+               new{ name = "hasan", age = 52 } ,
+               new{ name = "rashed", age = 21 } ,
+               new{ name = "monir", age = 45 } ,
+
+        
+
+   
         };
 
+     dynamic orders = new[]
+    {
+            new{ name = "monir", amount = 300.5 } ,
+           new{ name = "rashed", amount = 20.5 },
+           new{ name = "tareq", amount = 29.9 },
+           new{ name = "hasan", amount = 17.7 },
+           new{ name = "jalaluddin", amount = 30.2 },
+          
+        };
         public List<(string, double)> GetOrders()
         {
             var result = new List<(string, double)>();
