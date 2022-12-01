c8a3c37263a2cddbd5fd44a7c1077167ed93889c
Shahoriar Ratul <Shahoriar.ratul@gmail.com>
2022-12-01 23:27:31 +0600

    last

---
+++ Exam2/Task4/methodes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal interface methodes
+    {
+        public string? GetShortName();
+        public abstract string GenerateQRCode();
+        public abstract string GetDiscount(double discountPercent);
+
+
+    }
+}
